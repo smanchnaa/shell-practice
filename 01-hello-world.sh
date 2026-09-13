e23f3fb1386d3680bb2edf42f2e3cad8181382b0
i_am_manchana <srikanth.manchana@dieboldnixdorf.com>
2026-09-13 14:01:12 +0530

    Update 01-hello-world.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,4 +3,4 @@
 # This is the commnet , this will not be executed by bash. This is only information purpose
 echo "Hello World"
 
-echo "Hi, This is Srikanth manchana"
+echo "Hi, This is Srikanth Manchana"
